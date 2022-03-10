c103a7ec3662ae4ad3ac61d7c0a3e867acef9468
bob <66337541+Edu-Kobus@users.noreply.github.com>
2022-03-09 22:53:10 -0300

    sample 1 - duplication code

--- cypress/integration/duplication/sample1.spec.js
+++ cypress/integration/duplication/sample1.spec.js
@@ -1,5 +1,6 @@
 describe('Code duplication bad practice - Sample 1', () => {
-  it('searches by typing and hitting enter', () => {
+
+  beforeEach(() => {
     cy.intercept(
       'GET',
       '**/search**'
@@ -9,39 +10,30 @@ describe('Code duplication bad practice - Sample 1', () => {
     cy.wait('@getStories')
 
     cy.get('input[type="text"]')
+      .as('searchField')
       .should('be.visible')
       .and('have.value', 'redux')
       .clear()
-      .type('frontend testing{enter}')
-
+  });
+  
+  afterEach(() => {
     cy.wait('@getStories')
 
     cy.get('.table-row')
       .should('have.length', 100)
+  });
+
+  it('searches by typing and hitting enter', () => {
+    cy.get('@searchField')
+      .type('frontend testing{enter}')
   })
 
   it('searches by typing and pressing the search button', () => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
-
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-
-    cy.get('input[type="text"]')
-      .should('be.visible')
-      .and('have.value', 'redux')
-      .clear()
+    cy.get('@searchField')
       .type('frontend testing')
 
     cy.contains('button', 'Search')
       .should('be.visible')
       .click()
-
-    cy.wait('@getStories')
-
-    cy.get('.table-row')
-      .should('have.length', 100)
   })
 })
